682d515670ea858cb2958c0b9e8fb9fcd709655e
apurv roy <apurvroy010@gmail.com>
2019-11-28 17:35:36 +0530

    make a social website

---
+++ config/environment.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const path = require('path');
+
+
+const logDirectory = path.join(__dirname, '../production_logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs('access.log', {
+    interval: '1d',
+    path: logDirectory
+});
+
+
+
+const development = {
+    
+    name: 'development',
+    asset_path: './assets',
+    session_cookie_key: 'rDqyJ5hVS2rRP823Xe8BByBqMI1Ciz2i',
+    db: 'my_codeial_development',
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CODEIAL_GMAIL_USERNAME,
+            pass: process.env.CODEIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: "333012448622-enb6m5ccc1mpu63chr8lv512r47pjl7t.apps.googleusercontent.com",
+    google_client_secret: "uIgvSBdksgnEgf9ngmFv2SJH",
+    google_call_back_url: "http://localhost:8000/users/auth/google/callback",
+    jwt_secret: 'kAHGiZCiYYSrj3YZNOvlnce1nMyawv8u',
+    morgan: {
+        mode: 'dev',
+        options: {stream: accessLogStream}
+    },
+    
+}
+
+
+const production =  {
+    name: 'production',
+    asset_path: process.env.CODEIAL_ASSET_PATH,
+    session_cookie_key: process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODEIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CODEIAL_GMAIL_USERNAME,
+            pass: process.env.CODEIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_call_back_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+    jwt_secret: process.env.CODEIAL_JWT_SECRET,
+    morgan: {
+        mode: 'combined',
+        options: {stream: accessLogStream}
+    }
+}
+
+
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT)==undefined?development:eval(process.env.CODEIAL_ENVIRONMENT);
